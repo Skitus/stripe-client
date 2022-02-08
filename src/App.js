b97811d99a96bafccd5bd6b3be9a994d6889171a
Skitus <artur.demenskiy03@gmail.com>
2022-02-08 19:27:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Components
+import HomePage from './components/HomePage.js';
+// Stripe
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+// Styles
+import './index.scss';
+
+const stripePromise = loadStripe("pk_test_51KQpaVKtH21p71NufC2oknPHjMdrsZfW0SD6Brt5W7smxWcl1P779jarf34sC1notbjgj40s0eDnSj27yEZauMFU00yvUsBjiG");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Elements stripe={stripePromise}>
+        <HomePage />
+      </Elements>
   );
 }
 
